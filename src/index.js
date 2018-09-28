dccb8de092b15bcf6463986e61780f89a8ae4d55
Meearlyam <shavela99@gmail.com>
2018-09-28 21:11:18 +0300

    unfortunately takes a lot of time...

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let twos = 0, fives = 0, temp;
+  if(number <= 3) {
+    return count;
+  }
+  for (let i = number; i > 1; i--){
+    temp = i;
+    while(Math.floor(temp % 5) == 0) {
+      temp = Math.floor(temp/5);
+      fives++;
+    }
+    while(Math.floor(temp % 2) == 0) {
+      temp = Math.floor(temp/2);
+      twos++;
+    }
+  }
+  return Math.min(fives, twos);
 }
